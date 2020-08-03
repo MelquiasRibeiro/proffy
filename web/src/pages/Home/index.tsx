a4fdaa79ff1d628693e4e11ed1d06c1303c6c1a5
Melquias Ribeiro <melquias10.mr@gmail.com>
2020-08-03 17:15:12 -0300

    feat: home page

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,7 +1,46 @@
 import React from 'react';
+import './style.css'
+import Logo from '../../assets/logo.svg'
+import Landing from '../../assets/landing.svg'
+import StudyIcon from '../../assets/icons/study.svg'
+import GiveClassesIcon from '../../assets/icons/give-classes.svg'
+import PurpleHeartIcon from '../../assets/icons/purple-heart.svg'
 
 const Home: React.FC = () => {
-  return (<h1>Home</h1>);
+  return (
+    <div id="page-landing">
+      <div id="page-landing-content" className="container">
+        <div className="logo-container">
+          <img src={Logo} alt="Proffy"/>
+          <h2>Sua plataforma de estudos online.</h2>
+        </div>
+
+          <img 
+            src={Landing} 
+            alt="" 
+            className="hero-image"
+          />
+
+          <div className="buttons-container">
+            <a href="/" className="study">
+              <img src={StudyIcon}  alt="study"/>
+              Estudar
+            </a>
+
+            <a href="/" className="give-classes">
+              <img src={GiveClassesIcon}  alt="study"/>
+              Dar Aulas
+            </a>
+          </div>
+
+          <span className="total-conections">
+            Total de 200 conexões realizadas {" "}
+            <img src={PurpleHeartIcon} alt=""/>
+          </span>
+          
+        </div>
+    </div>
+    );
 }
 
 export default Home;
