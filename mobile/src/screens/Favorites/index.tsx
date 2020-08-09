af3300b8fcc10b4608f668a27bf451723b8e8e01
Melquias Ribeiro <melquias10.mr@gmail.com>
2020-08-09 13:20:46 -0300

    feat: created a header component

--- mobile/src/screens/Favorites/index.tsx
+++ mobile/src/screens/Favorites/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { View } from 'react-native';
 
 import styles from './styles';
+import Header from '../../components/Header';
 
 const Favorites: React.FC = () => {
   return (
     <View style={styles.container}>
-  
+        <Header title="Seus Proffys
+        Favoritos"/>
     </View>
       
 )}
--- mobile/src/screens/TeacherList/index.tsx
+++ mobile/src/screens/TeacherList/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import styles from './styles';
+import Header from '../../components/Header';
 
 const TeacherList: React.FC = () => {
     return (
     <View style={styles.container}>
-        
+        <Header title="Proffys Disponíveis"/>
     </View>
     
 )}
